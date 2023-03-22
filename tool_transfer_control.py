2e6dbc8e44601961805818ce1767335ef4f88d9c
EVERYSAN <128579465+EVERYSAN@users.noreply.github.com>
2023-03-22 16:55:15 +0900

    Update tool_transfer_control.py

--- tool_transfer_control.py
+++ tool_transfer_control.py
@@ -1,7 +1,7 @@
 path_sd15 = './models/v1-5-pruned.ckpt'
 path_sd15_with_control = './models/control_sd15_openpose.pth'
-path_input = './models/anything-v3-full.safetensors'
-path_output = './models/control_any3_openpose.pth'
+path_input = './models/chilloutmix_NiPrunedFp32Fix.safetensors'
+path_output = './models/control_chilloutmix_NiPrunedFp32Fix_openpose.pth'
 
 
 import os
